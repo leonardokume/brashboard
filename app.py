f6ed65384e803a355c45780d01c082b8072c86d0
Leonardo Kume <leokume28@gmail.com>
2020-06-29 22:53:39 -0300

    Added Google Analytics tag

--- app.py
+++ app.py
@@ -480,6 +480,35 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.UNITED],
 
 app.title = 'Brashboard: situação do coronavírus nas cidades'
 
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+       <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-171052480-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-171052480-1');
+        </script>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+'''
+
 server = app.server
 
 app.layout = dbc.Container([NAVBAR, BODY], fluid=True, style={'padding-right':'0px', 'padding-left':'0px'})
